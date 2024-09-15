9527dbf35c82527cc6cb82c2f47ee6aee0b871d0
gcdavis26 <geoffreycdavis@proton.me>
2024-09-15 16:53:55 -0400

    reduced code into for loop

--- problem1.m
+++ problem1.m
@@ -2,39 +2,13 @@ clc
 clear
 B = load('images.mat').image_data';
 B = mat2gray(B);
-firstimage = reshape(B(:,1), [37,50])';
-imshow(firstimage)
 
-secondimage = reshape(B(:,2), [37,50])';
-thirdimage = reshape(B(:,3), [37,50])';
-fourthimage = reshape(B(:,4), [37,50])';
-fifthimage = reshape(B(:,5), [37,50])';
-sixthimage = reshape(B(:,6), [37,50])';
-seventhimage = reshape(B(:,7), [37,50])';
-eigthimage = reshape(B(:,8), [37,50])';
-ninthimage = reshape(B(:,9), [37,50])';
-tenthimage = reshape(B(:,10), [37,50])';
+for iter = (1:10)
+    image = reshape(B(:,iter), [37, 50])'
+    subplot(1,10, iter)
+    imshow(image)
 
-subplot(1,10,1)
-imshow(firstimage)
-subplot(1,10,2)
-imshow(secondimage)
-subplot(1,10,3)
-imshow(thirdimage)
-subplot(1,10,4)
-imshow(fourthimage)
-subplot(1,10,5)
-imshow(fifthimage)
-subplot(1,10,6)
-imshow(sixthimage)
-subplot(1,10,7)
-imshow(seventhimage)
-subplot(1,10,8)
-imshow(eigthimage)
-subplot(1,10,9)
-imshow(ninthimage)
-subplot(1,10,10)
-imshow(tenthimage)
+end
 
 A = B * B';
 C = eig(A);
