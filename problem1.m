cdf0a0a0b5e27faf962742c4e468e321365cb2ae
gcdavis26 <geoffreycdavis@proton.me>
2024-09-15 18:27:22 -0400

    Added part of 1.3

--- problem1.m
+++ problem1.m
@@ -6,7 +6,7 @@ B = mat2gray(B);
 
 
 for iter = (1:10)
-    image = reshape(B(:,iter), [37, 50])'
+    image = reshape(B(:,iter), [37, 50])';
     subplot(1,10, iter)
     imshow(image)
 
@@ -14,11 +14,37 @@ end
 
 A = B * B';
 [Evectors, Evalues] =  eig(A);
-Evalues = diag(Evalues)
+origvect = Evectors;
+origval = Evalues;
+
+Evalues = diag(Evalues);
 Evalues = Evalues(563:1850,1);
 Evectors = Evectors(:,563:1850);
 Evalues = flip(Evalues);
-Evectors = flip(Evectors);
+Evectors = (flip(Evectors'))';
 figure(2)
 loglog(Evalues)
+
+%Smallest eigenvalues 
+figure(3)
+
+smallest = Evectors(:, [1288-9:1288]);
+
+for iter = (1:10)
+    image = reshape(smallest(:,iter), [37,50])'
+    image = mat2gray(image)
+    subplot(1,10,iter)
+    imshow(image)
+
+end
+
+figure(4)
+largest = Evectors(:,1:10)
+for iter = (1:10)
+    image = reshape(largest(:,iter), [37,50])'
+    image = mat2gray(image)
+    subplot(1,10,iter)
+    imshow(image)
+end
+
 
